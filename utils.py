48622247bc73d2824eee295eae1583b665ecff10
Eero Helenius <eerohele@users.noreply.github.com>
2017-03-29 10:21:06 +0300

    Remove commented code

--- utils.py
+++ utils.py
@@ -10,12 +10,6 @@ def is_relative_path(url):
     u = urlparse(url)
     return not bool(u.scheme) and not bool(u.netloc) and not os.path.isabs(url)
 
-# def resolve_file_path(path, against):
-#     if against and is_relative_path(path):
-#         return urljoin("file:", urljoin(against, path))
-#     else:
-#         return path
-
 
 def resolve_file_path(path, against):
     if against and is_relative_path(path):
